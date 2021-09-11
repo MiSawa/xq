f76ba724b6c9e1b7450798936f3ef81d65cbaebe
mi_sawa <mi.sawa.1216+git@gmail.com>
2021-09-11 21:21:20 +0900

    verbosity

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,14 +2,18 @@ use anyhow::{Context, Result};
 use clap::{AppSettings, Clap, ValueHint};
 use std::path::PathBuf;
 
-#[derive(Clap)]
+#[derive(Clap, Debug)]
 #[clap(author, about, version)]
 #[clap(long_version(option_env!("LONG_VERSION").unwrap_or(env!("CARGO_PKG_VERSION"))))]
 #[clap(setting(AppSettings::ColoredHelp))]
 struct Args {
+    /// The query to run
     #[clap(default_value("."))]
     query: String,
+
+    /// Read query from a file instead of arg
     #[clap(
+        name("file"),
         short('f'),
         long("from-file"),
         parse(from_os_str),
@@ -17,10 +21,17 @@ struct Args {
         value_hint(ValueHint::FilePath)
     )]
     query_file: Option<PathBuf>,
+
+    /// Verbose mode (-v, -vv, -vvv, etc.)
+    #[clap(short('v'), long("verbose"), parse(from_occurrences))]
+    verbosity: u8,
 }
 
 fn main() -> Result<()> {
     let args: Args = Args::parse();
+    if args.verbosity > 2 {
+        eprintln!("{:?}", args);
+    }
     let query = if let Some(path) = args.query_file {
         std::fs::read_to_string(path)?
     } else {
