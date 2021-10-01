a55cbd8f327860620aac46adc51833f20104a90d
mi_sawa <mi.sawa.1216+git@gmail.com>
2021-10-01 23:58:52 +0900

    Add recursive closure call

--- tests/hand_written/mod.rs
+++ tests/hand_written/mod.rs
@@ -1,1 +1,20 @@
+use crate::test;
+
 mod assignments;
+
+test!(
+    recursive,
+    r#"
+    def f: def g(x): ., x; g((. + 1) as $v | $v | if . > 2 then empty else f, $v end); f
+    "#,
+    r#"
+    null
+    "#,
+    r#"
+    null
+    1
+    2
+    2
+    1
+    "#
+);
