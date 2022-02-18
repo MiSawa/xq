1f340d7fcf4ff0de1d68cc74d709b06dbfe74a11
mi_sawa <mi.sawa.1216+git@gmail.com>
2022-02-19 03:29:02 +0900

    Error handling

--- src/lang/mod.rs
+++ src/lang/mod.rs
@@ -8,11 +8,20 @@ use thiserror::Error;
 lalrpop_mod!(#[allow(clippy::all, unused_imports)] pub parser, "/lang2/jq.rs");
 
 pub type ParseResult<T> = Result<T, ParseError>;
+type Loc = lexer::Loc;
 
 #[derive(Debug, Error)]
-#[error("{message}")]
-pub struct ParseError {
-    message: String,
+pub enum ParseError {
+    #[error("Got an invalid token at {0:?}")]
+    InvalidToken(Loc),
+    #[error("Got an EOF at {0:?}, but expected one of {1:?}")]
+    UnrecognizedEOF(Loc, Vec<String>),
+    #[error("Got a token {1} at {0:?}, but expected one of {2:?}")]
+    UnrecognizedToken(Loc, String, Vec<String>),
+    #[error("Got an unexpected additional token {0}")]
+    ExtraToken(String),
+    #[error("{0}")]
+    LexerError(String),
 }
 impl<'a> From<lalrpop_util::ParseError<lexgen_util::Loc, lexer::Token<'a>, lexer::LexerError>>
     for ParseError
@@ -20,8 +29,19 @@ impl<'a> From<lalrpop_util::ParseError<lexgen_util::Loc, lexer::Token<'a>, lexer
     fn from(
         e: lalrpop_util::ParseError<lexgen_util::Loc, lexer::Token<'a>, lexer::LexerError>,
     ) -> Self {
-        Self {
-            message: format!("{e:?}"),
+        match e {
+            lalrpop_util::ParseError::InvalidToken { location } => Self::InvalidToken(location),
+            lalrpop_util::ParseError::UnrecognizedEOF { location, expected } => {
+                Self::UnrecognizedEOF(location, expected)
+            }
+            lalrpop_util::ParseError::UnrecognizedToken {
+                token: (loc, token, _),
+                expected,
+            } => Self::UnrecognizedToken(loc, format!("{token:?}"), expected),
+            lalrpop_util::ParseError::ExtraToken { token } => {
+                Self::ExtraToken(format!("{token:?}"))
+            }
+            lalrpop_util::ParseError::User { error } => Self::LexerError(format!("{error:?}")),
         }
     }
 }
@@ -35,11 +55,13 @@ pub fn parse_program(input: &str) -> ParseResult<ast::Program> {
 
 #[cfg(test)]
 mod test {
-    use super::{ast::Query, lexer::Lexer, parser::QueryParser, ParseResult};
+    use super::{ast, lexer, parser, ParseResult};
 
-    fn parse_query(q: &str) -> ParseResult<Query> {
-        let lexer = Lexer::new(q);
-        QueryParser::new().parse(q, lexer).map_err(|e| e.into())
+    fn parse_query(q: &str) -> ParseResult<ast::Query> {
+        let lexer = lexer::Lexer::new(q);
+        parser::QueryParser::new()
+            .parse(q, lexer)
+            .map_err(|e| e.into())
     }
 
     #[test]
@@ -131,8 +153,11 @@ mod test {
     }
 
     #[test]
-    fn test_num() -> ParseResult<()> {
-        assert!(parse_query("2").is_ok());
+    fn test_other() -> ParseResult<()> {
+        parse_query("2")?;
+        parse_query(".[]")?;
+        println!("{:?}", parse_query("[][]")?);
+        parse_query("f(.; .)[]")?;
         Ok(())
     }
 }
