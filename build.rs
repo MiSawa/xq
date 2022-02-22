1fa4dfab9813f1cf87c3a170948836ce3f06c8c8
mi_sawa <mi.sawa.1216+git@gmail.com>
2022-02-22 10:13:45 +0900

    Handle erroneous case of the process baking long version

--- build.rs
+++ build.rs
@@ -4,17 +4,15 @@ fn main() {
     lalrpop::process_root().unwrap();
 
     let package_version = env!("CARGO_PKG_VERSION");
-    let git_output = Command::new("git")
+    let git_revision = Command::new("git")
         .args(&["rev-parse", "HEAD"])
         .output()
-        .expect("Failed to execute command `git rev-parse HEAD`")
-        .stdout;
-    let git_revision = String::from_utf8(git_output)
-        .expect("Unable to interpret the output of `git rev-parse HEAD` as an UTF-8 string")
-        .trim()
-        .to_string();
-    println!(
-        "cargo:rustc-env=LONG_VERSION={}-{}",
-        package_version, git_revision
-    );
+        .ok()
+        .and_then(|o| String::from_utf8(o.stdout).ok())
+        .map(|s| s.trim().to_string());
+    if let Some(git_revision) = git_revision {
+        println!("cargo:rustc-env=LONG_VERSION={package_version}-{git_revision}");
+    } else {
+        println!("cargo:rustc-env=LONG_VERSION={package_version}");
+    }
 }
